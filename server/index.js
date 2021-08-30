fed9b714a26cbd01c54653058257668d845506af
Alao-Abiodun <46402042+Alao-Abiodun@users.noreply.github.com>
2021-08-30 17:56:46 +0100

    feat/setup a web socket.io on the server

--- server/index.js
+++ server/index.js
@@ -1,30 +1,46 @@
-const path = require('path');
 const express = require('express');
-
+const path = require('path');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
+// // Have Node serve the files for our built React app
+// // app.use(express.static(path.resolve(__dirname, '../client/build')));
 
+io.on('connection', (socket) => {
+  console.log('Websocket connected!!!');
 
-// Node API Endpoints
-app.get("/ping", (req, res) => {
-    res.json({ message: "Hello from server!" });
+  socket.emit('Game Room');
+
 });
+
+// // Node API Endpoints
+// app.get("/ping", (req, res) => {
+//     res.json({ message: "Hello from server!" });
+// });
+
 // Node API Endpoints
-
-
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// });
 
 // Set Port
 const PORT = process.env.PORT || 5000;
 
 // Listen to port
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server started listening on port ${PORT}`);
 });
+
+
+
+
+
+
+
