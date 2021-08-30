2e0f029fb0c05eaea0206f46b17ce4722adca9df
Eniola Osabiya <eosabiya@gmail.com>
2021-08-30 03:09:49 +0100

    react server handler

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
-// Express Server Code will be here
+const path = require('path');
 const express = require('express');
 
 const app = express();
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+
+
+// Node API Endpoints
+app.get("/ping", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+// Node API Endpoints
+
+
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 // Set Port
 const PORT = process.env.PORT || 5000;
