3cc087d6bb3c8eca593758f60e68ed8b53b94410
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-08-30 20:21:49 +0100

    Set up routing for pages with react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,41 @@
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch
+} from 'react-router-dom';
+
 import "./App.css";
+import AdvancedSetting from './Pages/Advanced_Setting/AdvancedSetting'
+import MainGame from './Pages/GameScreen/MainGame'
+import Homepage from './Pages/Homepage.js/Homepage'
+import InviteNotification from './Pages/InviteNotification/InviteNotification'
+import LeaderBoard from './Pages/LeaderBoard/LeaderBoard'
+import MainMenu from './Pages/MainMenu/MainMenu'
+import PlayFriend from './Pages/Play_With_A_Friend/PlayFriend'
+import QuickSetting from './Pages/Quick_Setting/QuickSetting'
+import Playsettings from './Pages/Settings_Before_Play/Playsettings'
+import Notification from './Pages/TagAndMention/Notification'
+import Tournament from './Pages/Tournament/Tournament'
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path='/advanced-setting' component={AdvancedSetting}/>
+          <Route exact path='/main-game' component={MainGame}/>
+          <Route exact path='/home' component={Homepage}/>
+          <Route exact path='/invite-notification' component={InviteNotification}/>
+          <Route exact path='/leader-board' component={LeaderBoard}/>
+          <Route exact path='/main-menu' component={MainMenu}/>
+          <Route exact path='/play-with-a-friend' component={PlayFriend}/>
+          <Route exact path='/quick-setting' component={QuickSetting}/>
+          <Route exact path='/play-settings' component={Playsettings}/>
+          <Route exact path='/tag-and-mention' component={Notification}/>
+          <Route exact path='/tournament' component={Tournament}/>
+        </Switch>
+      </Router>
+
       <header class="main-header">
         <nav>
           <h1 id="logo">Chess Board Room Plugin</h1>
