cb48cc2cca37e67bd26e068b97901463ea39b64f
Eniola Osabiya <eosabiya@gmail.com>
2021-08-31 08:24:57 +0100

    new url to test backend work, keeping the react app on the main domain

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const http = require("http");
 const express = require("express");
+
 const { Server } = require("socket.io");
 const { userJoin } = require("./utils/users");
 const formatMessage = require("./utils/chessMessage");
@@ -11,12 +12,13 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 // // Have Node serve the files for our built React app
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
+// For Testing Backend Socket.io
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
-  console.log("Websocket connected!!!"); 
+  console.log("Websocket connected!!!");
 
   socket.on(
     "joinRoom",
@@ -55,6 +57,7 @@ app.get("/info", (req, res) => {
   } catch (e) {
     res.status(500).send("Could not fetch plugin information");
   }
+});
 
 // sidebars Endpoint
 app.get("/sidebars", (req, res) => {
@@ -68,24 +71,30 @@ app.get("/sidebars", (req, res) => {
     joinTournament: "/api/chess/tournament/join",
     settings: "/api/chess/settings",
     join: "/api/chess/join-live"
-}
-  
+  }
+
   res.status(200).json(sidebars)
-})
+});
 
 app.get('/info', (req, res) => {
-    try {
-        res.status(200).json({ plugin: "plugin for Zuri Chat that enables the users play chess within the application" });
-    } catch (e) {
-        res.status(500).send("Could not fetch plugin information");
-    }
+  try {
+    res.status(200).json({ plugin: "plugin for Zuri Chat that enables the users play chess within the application" });
+  } catch (e) {
+    res.status(500).send("Could not fetch plugin information");
+  }
 });
 // Node API Endpoints
 
-// // All other GET requests not handled before will return our React app
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-// });
+
+// All url requests with prefix /test will be handled by the public folder
+app.get('/test/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 // Set Port
 const PORT = process.env.PORT || 5000;
