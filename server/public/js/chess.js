b111c1b3ccc58f228da2eef83d518f4c36b458a4
Destiny Erhabor <destinterhabor6@gmail.com>
2021-08-31 04:42:29 +0100

    modify the static public file to display message emitted from the server socket in the dom

--- server/public/js/chess.js
+++ server/public/js/chess.js
@@ -1,5 +1,16 @@
 const socket = io();
 
-socket.on('Game Room', () => {
-    console.log('Game Room Created Successfully...');
+socket.on('message', message =>{
+    console.log(message);
+    outputMessage(message);
 })
+
+// Join Game Room
+socket.emit('joinRoom', {username:'Player 2', gameRoom: 'test'});
+
+
+// Output message
+function outputMessage(join) {
+  const plyerJoined = document.querySelector("#player_join");
+  plyerJoined.innerHTML += `${join.text} <br/>`;
+}
