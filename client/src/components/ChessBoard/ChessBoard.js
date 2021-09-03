6f5251e96010214dec55809c2a764286a4dffcdc
Adigun Idowu Michael <idowumichael2509@gmail.com>
2021-09-04 00:29:21 +0100

    implementation of chess piece logic/move

--- client/src/components/ChessBoard/ChessBoard.js
+++ client/src/components/ChessBoard/ChessBoard.js
@@ -1,10 +1,31 @@
 import './ChessBoard.css';
 import Chessboard from "chessboardjsx";
+import React, { useState, useEffect, useRef } from "react"
+import Chess from "chess.js"
 
 const ChessBoard = () => {
+    const [fen, setFen] = useState("start")
+
+    let game = useRef(null);
+  
+    useEffect(() => {
+      game.current = new Chess();
+    }, [])
+  
+    const onDrop = ({sourceSquare, targetSquare}) => {
+      let move = game.current.move({
+        from: sourceSquare,
+        to: targetSquare
+      })
+  
+      if(move === null) return;
+  
+      setFen(game.current.fen())
+    }
+
     return ( 
         <div className="chessboard">
-            <Chessboard width = { 400 } id = "startPos" position="start"/>
+            <Chessboard width = { 400 } id = "startPos" position={ fen } onDrop = {onDrop} />
         </div>
      );
 }
