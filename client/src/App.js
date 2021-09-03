c717a40a8e8d2787c6948dee3efaf17fd585ae19
Adaeze Ndupu <andupu4@gmail.com>
2021-09-03 14:25:12 +0000

    imported home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import "./App.css";
 import HomePage from "./Pages/Homepage.js/Homepage"
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
 
      <HomePage />
-     
+
     </div>
   );
 }
