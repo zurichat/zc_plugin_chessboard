3cb01a032f5cae2c329a7e507b1690dbc677d334
Odizee <66241926+odizee@users.noreply.github.com>
2021-09-03 06:56:52 -1100

    Revert "Worked on Player's game page"

--- client/src/Pages/Homepage/Homepage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import MiniBoard from "../../components/MiniBoard/MiniBoard";
-
-export default function Homepage() {
-  return <div>
-    <MiniBoard id="234" playerOne="Annietah" playerTwo="codeJonin" />
-      <MiniBoard id="234" playerOne="" playerTwo="codeJonin" />
-  </div>
-}
