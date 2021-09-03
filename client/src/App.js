dcc9816449bc10bb49f08a0fbcd04822eee6fbfd
Adaeze Ndupu <andupu4@gmail.com>
2021-09-03 14:29:34 +0000

    committ

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
+import "./App.css";
+
 import HomePage from "./Pages/Homepage.js/Homepage"
 
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
      <HomePage />
-
     </div>
   );
 }
