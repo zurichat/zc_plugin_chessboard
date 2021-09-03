498cc9b852bc34f2a59daf1b82d85ca29af1c07a
Odizee <66241926+odizee@users.noreply.github.com>
2021-09-03 06:08:39 -1100

    Revert "Dev"

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
 import "./App.css";
-
-
-
+import HomePage from "./Pages/Homepage.js/Homepage"
+import MainGame from "./Pages/GameScreen/MainGame"
 
 function App() {
   return (
     <div className="App">
-      <header class="main-header">
-        <nav>
-          <h1 id="logo">Chess Board Room Plugin</h1>
-        </nav>
-      </header>
-      <section class="hero">
-        <h2>Welcome to the Chess Board Room</h2>
-
-        <button>Start Game</button>
-      </section>   
-      
+      <Router>
+        <Switch>
+          <Route exact path='/' render={HomePage} />
+          <Route exact path='/game' render={MainGame} />
+        </Switch>
+      </Router>
+     {/* <HomePage /> */}
     </div>
   );
 }
