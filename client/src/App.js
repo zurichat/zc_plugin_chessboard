54a7e6b01a1031d9bc1bcfa265bd7d5c87292ec9
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-09-03 16:18:55 +0100

    react router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
 import "./App.css";
 import HomePage from "./Pages/Homepage.js/Homepage"
+import MainGame from "./Pages/GameScreen/MainGame"
 
 function App() {
   return (
     <div className="App">
-     <HomePage />
+      <Router>
+        <Switch>
+          <Route exact path='/' render={HomePage} />
+          <Route exact path='/game' render={MainGame} />
+        </Switch>
+      </Router>
+     {/* <HomePage /> */}
     </div>
   );
 }
