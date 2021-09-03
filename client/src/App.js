2a57e824412cd0bec0f5adfb27f77735415022f2
Anita9771 <ieghonghon@gmail.com>
2021-09-03 11:29:01 +0100

    second commit for Chessboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,20 @@
 import "./App.css";
-import React, { Component } from 'react'
-import {Link} from 'react-router-dom';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ChessBoard from './components/ChessBoard/ChessBoard';
 
-class App extends Component {
-  render(){
-    return (
-      <Router>
-        <div className="App">
-        <header class="main-header">
-          <nav>
-            <h1 id="logo">Chess Board Room Plugin</h1>
-          </nav>
-        </header>
-        <section class="hero">
-          <h2>Welcome to the Chess Board Update</h2>
-  
-          <button><Link>Start Game</Link></button>
-  
-        </section>
-      
-      <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/chessboard" component={ChessBoard} />
-      </Switch>
-      </div>
-    </Router>
+function App() {
+  return (
+    <div className="App">
+      <header class="main-header">
+        <nav>
+          <h1 id="logo">Chess Board Room Plugin</h1>
+        </nav>
+      </header>
+      <section class="hero">
+        <h2>Welcome to the Chess Board Room</h2>
 
-      
-    );
-  }
-    
+        <button>Start Game</button>
+      </section>
+    </div>
+  );
 }
 
 export default App;
