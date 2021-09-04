254044d75ae3de6e7d8a622eb4983f37b8831eb9
odizee <odizee@users.noreply.github.com>
2021-09-04 14:24:38 +0000

    same_commit

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,12 +1,12 @@
 import Input from "./Input/Input";
 import "./Chat.css";
-import Comment from "../Chat/Comment/Chat"
+import Comment from "../Chat/Comment/Chat";
 
 const Chat = () => {
   return (
     <div id="chat">
       <h1>Chat</h1>
-       <Comment />
+      <Comment />
       <Input />
     </div>
   );
