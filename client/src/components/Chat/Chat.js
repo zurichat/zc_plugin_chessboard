61daa7981db40d96206aa83eb2bab0baa1416a24
Yemi-dev <akinyemitiana77@gmail.com>
2021-09-04 14:08:23 +0100

    add

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,12 +1,11 @@
 import Input from "./Input/Input";
 import "./Chat.css";
-import Comment from "../Comment/Chat";
+import Comment from "../Chat/Comment/Chat"
 
 const Chat = () => {
   return (
     <div id="chat">
       <h1>Chat</h1>
-      {/* yemyem... your component enters here */}
        <Comment />
       <Input />
     </div>
--- client/src/components/Chat/Comment/Chat.js
+++ client/src/components/Chat/Comment/Chat.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./index.css";
+import avi from "../../../assets/ChatAvatar.png"
 import moment from "moment";
-import avi from "../../assets/ChatAvatar.png";
+
 
 const data = {
   players: [
@@ -94,7 +95,7 @@ function Comment() {
         <p>Loading</p>
       )}
 
-      {players.includes(mainUser.id) ? null : (
+      {/* {players.includes(mainUser.id) ? null : (
         <>
           <div className="chatInputForm">
             <input
@@ -105,7 +106,7 @@ function Comment() {
             <button onClick={submitForm}>submit</button>
           </div>
         </>
-      )}
+      )} */}
     </div>
   );
 }
