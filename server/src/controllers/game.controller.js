cdcb6a10710e41d9b5c21a82e7156128550f3c36
KenDerick <chenemiken15@gmail.com>
2021-09-04 11:25:11 +0100

    setup schema

--- server/src/controllers/game.controller.js
+++ server/src/controllers/game.controller.js
@@ -8,21 +8,31 @@ const CustomError = require("../utils/custom-error");
 class GameController {
     async create(req, res) {
         try {
-
             let game_id = uuid.v4();
-            let game_options = {
-                game_owner_user_id: uuid.v4(),
-                player_color: 'b',
+            var date_ob = new Date();
+            var game_owner_id = "playerX"
+            var opponent_id = "playerY"
+            console.log(date_ob)
+            var new_game =  {
+                game_id: game_id,
+                game_owner_user_id: game_owner_id,
+                opponent_user_id: opponent_id,
+                start_time: date_ob,
+                end_time: null,
+                // moves: [
+                //     {game_owner_id,board_state},
+                //     {opponent_id,board_state}
+                // ],
+                result_id: null
             }
 
             // Save the new game in DB
             // db code here
-
-            res.status(201).send(response("Plugin Information Retrieved", {
-                game_id, game_options
-            }));
+            res.status(201).send(response("New Game Created Successfully", {new_game}));
         } catch (error) {
+            console.log(error);
             throw new CustomError("Could not create a new game", "500");
+            
         }
     }
 }
