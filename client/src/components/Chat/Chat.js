c3341582c5d3d2e6e393f12977af66e1cfcdb92b
Yemi-dev <akinyemitiana77@gmail.com>
2021-09-04 13:18:27 +0100

    added

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,12 +1,13 @@
 import Input from "./Input/Input";
 import "./Chat.css";
+import Comment from "../Comment/Chat";
 
 const Chat = () => {
   return (
     <div id="chat">
       <h1>Chat</h1>
       {/* yemyem... your component enters here */}
-
+       <Comment />
       <Input />
     </div>
   );
