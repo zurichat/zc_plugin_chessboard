695efe05be590e6cd5b22efe481f0a2bd44c5f5b
unknown <ozovehe09@gmail.com>
2021-09-04 09:08:14 +0100

    save player move

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,6 +4,7 @@ const router = require("express").Router();
 // Custom Modules
 const InfoCtrl = require("../controllers/info.controller");
 const GameCtrl = require("../controllers/game.controller");
+const { savePlayerMove } = require("../db/write");
 
 // Endpoints
 router.get("/info", InfoCtrl.getPluginInfo);
@@ -15,6 +16,18 @@ router.get("/createGame", GameCtrl.create);
 router.get("/ping", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
+
+router.get("/save-move", async(req, res) => {
+    const playerId = 23
+    const pieceId = 7
+    const initialPosition = 3
+    const finalPosition = 2
+    const gameId = '234djkla'
+
+    const response = await savePlayerMove({playerId, pieceId, initialPosition, finalPosition, gameId})
+
+    res.json({'message': 'success', data: response})
+})
 
 // Export Module
 module.exports = router
