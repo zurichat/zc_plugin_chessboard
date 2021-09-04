6f4058c66b5053cdcae23d002c37abf153f2400b
ESE-MONDAY <ESE-MONDAY@users.noreply.github.com>
2021-09-04 10:31:21 +0000

    same_commit

--- client/src/components/Modals/ModalPage/Modal.js
+++ client/src/components/Modals/ModalPage/Modal.js
@@ -1,15 +1,12 @@
-import React, {useState}  from "react";
+import React, { useState } from "react";
 import CongratulationsModal from "../CongratulationsModal/CongratulationsModal";
 
-
-export default function Modal(){
- const [show, setShow] = useState(false)
-    return(
-        <div>
-           
-            <button onClick={() => setShow(true)}>Show Congratulations Modal</button>
-            <CongratulationsModal onClose={() =>setShow(false)} show={show} />
-            
-        </div>
-    )
+export default function Modal() {
+  const [show, setShow] = useState(false);
+  return (
+    <div>
+      <button onClick={() => setShow(true)}>Show Congratulations Modal</button>
+      <CongratulationsModal onClose={() => setShow(false)} show={show} />
+    </div>
+  );
 }
