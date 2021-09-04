a052278d8b06e45cbaf81b0ec19470a8f85ebf37
Babsmutini <Babsmutini@users.noreply.github.com>
2021-09-04 15:19:01 +0000

    same_commit

--- client/src/components/Games/gamesection.js
+++ client/src/components/Games/gamesection.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './game.css';
-
-
+import "./game.css";
 
 import Chessboard from "../../assets/Chessboard.png";
 import Ellipse134 from "../../assets/Ellipse134.png";
@@ -12,32 +10,31 @@ import Ellipse136 from "../../assets/Ellipse136.png";
 
 function Gamesection() {
   return (
-   <div className = "container">
-     <figure className="position-relative">
-      <img src ={Ellipse134}  className="pic1" alt="ellipse"/> 
-     <div className= "text1"> @dejavu</div>
+    <div className="container">
+      <figure className="position-relative">
+        <img src={Ellipse134} className="pic1" alt="ellipse" />
+        <div className="text1"> @dejavu</div>
 
-     <div className="figcaption">
-    <img src ={Chessboard} alt="chessboard" className="img-fluid1"/>
-     <button className="btn-1"> Watch game</button>
-     </div>
-     
-     <img src ={Ellipse133} className="pic2" alt="ellipse"/>
-     <div className= "text2"> Techlead</div>
-     </figure>
-     <br></br>
-     <figure className="position-relative">
+        <div className="figcaption">
+          <img src={Chessboard} alt="chessboard" className="img-fluid1" />
+          <button className="btn-1"> Watch game</button>
+        </div>
 
-     <img src ={Ellipse135} className="pic3" alt="ellipse"/>
-     <div className= "text3"> @kemi</div>
-     <div className="figcaption">
-     <img src ={Chessboard} alt="chessboard" className="img-fluid2"/>
-     <button className="btn-2">Watch game</button>
-     </div>
-     <img src ={Ellipse136} className="pic4" alt="ellipse"/>
-     <div className= "text4"> Ademola</div>
-     </figure>
-     </div>
+        <img src={Ellipse133} className="pic2" alt="ellipse" />
+        <div className="text2"> Techlead</div>
+      </figure>
+      <br></br>
+      <figure className="position-relative">
+        <img src={Ellipse135} className="pic3" alt="ellipse" />
+        <div className="text3"> @kemi</div>
+        <div className="figcaption">
+          <img src={Chessboard} alt="chessboard" className="img-fluid2" />
+          <button className="btn-2">Watch game</button>
+        </div>
+        <img src={Ellipse136} className="pic4" alt="ellipse" />
+        <div className="text4"> Ademola</div>
+      </figure>
+    </div>
   );
 }
 
