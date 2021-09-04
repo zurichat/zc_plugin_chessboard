7d7f3c66fbca94fa930120a430aca105efb2e899
ESE-MONDAY <ESE-MONDAY@users.noreply.github.com>
2021-09-04 22:12:20 +0000

    same_commit

--- client/src/components/Modals/CongratulationsModal/CongratulationsModal.js
+++ client/src/components/Modals/CongratulationsModal/CongratulationsModal.js
@@ -1,37 +1,29 @@
-import React, { useState} from 'react'
-import Button from '../../Button/Button'
-import './CongratulationsModal.css';
+import React, { useState } from "react";
+import Button from "../../Button/Button";
+import "./CongratulationsModal.css";
 
-
-const CongratulationsModal = props => {
-
-  if (!props.show){
-    return null
+const CongratulationsModal = (props) => {
+  if (!props.show) {
+    return null;
   }
   return (
-      
-            <div className="container" onClick={props.onClose}>
+    <div className="container" onClick={props.onClose}>
+      <div className="modal">
+        <div className="modal-profile">
+          <div className="modal-img"></div>
+        </div>
+        <div className="modal-content">
+          <p>
+            <span>Congratulations</span> @EseMonday
+          </p>
+          <p>You won this round</p>
+        </div>
+        <div className="btn-group">
+          <Button />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-                    <div className='modal' >
-                      <div className='modal-profile'>
-                        <div className="modal-img"></div>
-
-                      </div>
-                      <div className="modal-content">
-                        <p><span>Congratulations</span> @EseMonday</p>
-                        <p>You won this round</p>
-
-                      </div>
-                      <div className="btn-group">
-                        <Button  />
-                      </div>
-                    
-                    </div>
-                  </div>
-
-      
-        
-    )
-}
-
-export default CongratulationsModal
+export default CongratulationsModal;
