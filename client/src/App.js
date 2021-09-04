fae76392ad45a1995594d480c67a22376dfc8588
kenogit78 <kenogit78@users.noreply.github.com>
2021-09-04 07:14:03 +0000

    same_commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,16 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import Games from './';
 
 import "./App.css";
-import HomePage from "./Pages/Homepage.js/Homepage"
-import MainGame from "./Pages/GameScreen/MainGame"
-
+import HomePage from "./Pages/Homepage.js/Homepage";
+import MainGame from "./Pages/GameScreen/MainGame";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path='/' render={HomePage} /> 
-          <Route exact path='/game' render={MainGame} />
-
+          <Route exact path="/" render={HomePage} />
+          <Route exact path="/game" render={MainGame} />
         </Switch>
       </Router>
     </div>
