6d0f8f49a3fe476b421b85bde0d30940b9d6a5e8
ESE-MONDAY <mondayese8@gmail.com>
2021-09-04 11:30:39 +0100

    congratulations modal

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import HomePage from "./Pages/Homepage.js/Homepage";
 import GameScreen2 from "./Pages/GameScreen2";
 import MainGame from "./Pages/GameScreen/MainGame.js";
+import Modal from "./components/Modals/ModalPage/Modal";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/game/games" render={MainGame} />
           <Route exact path="/game/chat" render={MainGame} />
           <Route exact path="/game_comments" render={GameScreen2} />
+          <Route exact path="/modalpage" render={Modal} />
         </Switch>
       </Router>
     </div>
---
+++ client/src/components/Modals/ModalPage/Modal.js
@@ -0,0 +1,15 @@
+import React, {useState}  from "react";
+import CongratulationsModal from "../CongratulationsModal/CongratulationsModal";
+
+
+export default function Modal(){
+ const [show, setShow] = useState(false)
+    return(
+        <div>
+           
+            <button onClick={() => setShow(true)}>Show Congratulations Modal</button>
+            <CongratulationsModal onClose={() =>setShow(false)} show={show} />
+            
+        </div>
+    )
+}
