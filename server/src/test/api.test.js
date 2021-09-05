1f047c28e91e12300dfa62d745d2b80ef3f64e21
tobybrave <tobybrave@users.noreply.github.com>
2021-09-05 09:14:32 +0000

    same_commit

--- server/src/test/api.test.js
+++ server/src/test/api.test.js
@@ -1,37 +1,36 @@
-const supertest = require("supertest")
-const app = require("../../index")
+const supertest = require("supertest");
+const app = require("../../index");
 
-const api = supertest(app)
+const api = supertest(app);
 
 test("random addition that passes", () => {
-  expect(2 + 2).toEqual(4)
-})
+  expect(2 + 2).toEqual(4);
+});
 
 test("health check", async () => {
-  const { body } = await api.get("/api/ping")
+  const { body } = await api
+    .get("/api/ping")
     .expect(200)
-    .expect("Content-Type", /application\/json/)
-    
-  expect(body)
-    .toMatchObject({
-      message: "Hello from server!"
-    })
-})
+    .expect("Content-Type", /application\/json/);
+
+  expect(body).toMatchObject({
+    message: "Hello from server!",
+  });
+});
 
 describe("Info controller", () => {
   test("GET /api/info", async () => {
-    const { body } = await api.get("/api/info")
+    const { body } = await api
+      .get("/api/info")
       .expect(200)
-      .expect("Content-Type", /application\/json/)
+      .expect("Content-Type", /application\/json/);
 
-    expect(body).toHaveProperty("message")
-    expect(body.data.type).toMatch(/plugin information/i)
-    expect(body.data.scaffold_structure).toBe("Monolith")
-    expect(body.data.team).toMatch(/team tesla$/i)
-    expect(body.data.homepage_url).toBe("https://chess.zuri.chat")
-    expect(body.data.plugin_info).toHaveProperty("name")
-    expect(body.data.plugin_info.description)
-      .toContain("Zuri.chat plugin")
-  }, 500)
-
-})
+    expect(body).toHaveProperty("message");
+    expect(body.data.type).toMatch(/plugin information/i);
+    expect(body.data.scaffold_structure).toBe("Monolith");
+    expect(body.data.team).toMatch(/team tesla$/i);
+    expect(body.data.homepage_url).toBe("https://chess.zuri.chat");
+    expect(body.data.plugin_info).toHaveProperty("name");
+    expect(body.data.plugin_info.description).toContain("Zuri.chat plugin");
+  }, 500);
+});
