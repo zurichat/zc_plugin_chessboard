7c1116216a9098fc78d5287a786203e79f4811d1
fabianuzukwu <fabianuzukwu@users.noreply.github.com>
2021-09-05 15:55:23 +0000

    same_commit

--- server/src/models/game.js
+++ server/src/models/game.js
@@ -4,7 +4,7 @@ const Joi = require("joi");
 //  a test user schema tobe updated based on requirements
 const gameSchema = Joi.object({
   game_id: Joi.string().guid({ version: "uuidv4" }).required,
-  timeDateCreated: Joi.date().default(new Date().toISOString())
+  timeDateCreated: Joi.date().default(new Date().toISOString()),
 });
 
 module.exports = gameSchema;
