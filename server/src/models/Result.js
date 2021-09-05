ee452b8b1d52873183ea84c6feab9dd5a4971178
kenny0619 <kehindeibukun0619@gmail.com>
2021-09-05 14:05:55 +0100

    [created an endpoint to update game with result in each game created in the db]-( fixes #431 )

--- server/src/models/Result.js
+++ server/src/models/Result.js
@@ -2,9 +2,9 @@ const Joi = require("joi");
 
 //  a test user schema tobe updated based on requirements
 const resultSchema = Joi.object({
-  result_id: Joi.string().guid({ version: "uuidv4" }).required,
-  game_id: Joi.string().guid({ version: "uuidv4" }).required,
-  winner_id: Joi.string().guid({ version: "uuidv4" }).required,
+  result_id: Joi.string().guid({ version: "uuidv4" }),
+  game_id: Joi.string().required(),
+  winner_id: Joi.string().required(),
 });
 
 module.exports = resultSchema;
