0958e4b899ad23a45016a549e212d8c0dfe3bde6
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2021-09-05 11:51:54 +0100

    added console loggin for centrifugo message

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 5050,
   SOCKER_KEY:
     process.env.SOCKER_TOKEN || "58c2400b-831d-411d-8fe8-31b6e337738b",
-  SOCKET_URL: process.env.SOCKET_URL || "wss://realtime.zuri.chat/api",
+  SOCKET_URL: process.env.SOCKET_URL || "https://realtime.zuri.chat/api",
 };
