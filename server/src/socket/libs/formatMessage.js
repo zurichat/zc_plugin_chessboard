c4e376e1b59ff8e40a3e5ca980cbda33fe2a89f3
Destiny Erhabor <destinterhabor6@gmail.com>
2021-09-06 11:33:39 +0100

    lint fixed

--- server/src/socket/libs/formatMessage.js
+++ server/src/socket/libs/formatMessage.js
@@ -1,8 +1,8 @@
 const formatMessage = (user_id, message) => {
-  return {
-    user_id,
-    message,
-  };
+    return {
+        user_id,
+        message,
+    };
 };
 
 module.exports = formatMessage;
