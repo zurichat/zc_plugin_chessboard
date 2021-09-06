e0188e24fc6bc6c22b0aa54ad475034c19434a60
Eniola Osabiya <eosabiya@gmail.com>
2021-09-06 17:46:34 +0100

    Game Model Complete

---
+++ server/src/models/game.model.js
@@ -0,0 +1,28 @@
+// Package Modules
+const Joi = require("joi");
+
+//  a test user schema tobe updated based on requirements
+const game_schema = Joi.object({
+    // Game ID is the default id by Mongoose "_id"
+    _id: Joi.string(),
+    // Game Owner User ID
+    game_owner_id: Joi.string().required(),
+    // Opponent User ID
+    game_opponent_user_id: Joi.string().allow(null),
+    // Game Start Time
+    game_start_time: Joi.date().required(),
+    // Game End Time
+    game_end_time: Joi.date().allow(null),
+    // Game Moves
+    game_moves: Joi.array().items(Joi.object({
+        // Player 0 - Game Owner, Player 1 - Opponent
+        player: Joi.number().required(),
+        // Board State
+        board_state: Joi.string().required(),
+    })),
+    game_result_id: [
+        Joi.string().allow(null),
+    ]
+});
+
+module.exports = game_schema;
--- server/src/socket/leave_room_handler.socket.js
+++
@@ -1,8 +0,0 @@
-// Custom Modules
-
-const leave_room_handler = (data) => {
-  console.log(data);
-  //Perform all server side logic for player leaving room
-};
-
-module.exports = leave_room_handler;
