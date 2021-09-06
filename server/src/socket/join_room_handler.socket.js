d48df3e5f1b3217ac9212c89bafebc3d40a5c72a
eni4sure <eni4sure@users.noreply.github.com>
2021-09-06 09:53:19 +0000

    Backend EsLint Fixes

--- server/src/socket/join_room_handler.socket.js
+++ server/src/socket/join_room_handler.socket.js
@@ -1,8 +1,8 @@
 // Custom Modules
 
 const join_room_handler = (data) => {
-  console.log(data);
-  // perform all server-side logic for player joining room
+    console.log(data);
+    // perform all server-side logic for player joining room
 };
 
 module.exports = join_room_handler;
