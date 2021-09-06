89045fd47e14fe046608c04fba08d4130b7e458c
Caesarsage <Caesarsage@users.noreply.github.com>
2021-09-06 10:33:58 +0000

    Backend EsLint Fixes

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,16 +2,16 @@ const Joi = require("joi");
 
 //  a test user schema tobe updated based on requirements
 const userSchema = Joi.object({
-  id: Joi.string().guid({ version: "uuidv4" }).required(),
-  userName: Joi.string().required(),
-  organization: [
-    Joi.string().guid({ version: "uuidv4" }).required(),
-    Joi.allow(null),
-  ],
-  wins: Joi.number().required(),
-  dateAdded: Joi.date().default(new Date().toISOString()),
-  dateModified: Joi.date().default(new Date().toISOString()),
-  lastAccessed: Joi.date().default(new Date().toISOString()),
+    id: Joi.string().guid({ version: "uuidv4" }).required(),
+    userName: Joi.string().required(),
+    organization: [
+        Joi.string().guid({ version: "uuidv4" }).required(),
+        Joi.allow(null),
+    ],
+    wins: Joi.number().required(),
+    dateAdded: Joi.date().default(new Date().toISOString()),
+    dateModified: Joi.date().default(new Date().toISOString()),
+    lastAccessed: Joi.date().default(new Date().toISOString()),
 });
 
 module.exports = userSchema;
