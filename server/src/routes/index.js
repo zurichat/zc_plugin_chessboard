2468e92b0a0a9715c3639d955cf5b10fc184b2ff
eni4sure <eni4sure@users.noreply.github.com>
2021-09-06 09:22:05 +0000

    same_commit

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,29 +1,29 @@
 // Package Modules
-const router = require('express').Router();
+const router = require("express").Router();
 
 // Custom Modules
-const InfoCtrl = require('../controllers/info.controller');
-const GameCtrl = require('../controllers/game.controller');
-const UserCtrl = require('../controllers/user.controller');
-const ResultCtrl = require('../controllers/result.controller');
+const InfoCtrl = require("../controllers/info.controller");
+const GameCtrl = require("../controllers/game.controller");
+const UserCtrl = require("../controllers/user.controller");
+const ResultCtrl = require("../controllers/result.controller");
 
 // Endpoints
-router.get('/info', InfoCtrl.getPluginInfo);
+router.get("/info", InfoCtrl.getPluginInfo);
 
-router.get('/sideBar', InfoCtrl.getSideBarInfo);
+router.get("/sideBar", InfoCtrl.getSideBarInfo);
 
-router.post('/createGame', GameCtrl.create);
-router.post('/joingame', GameCtrl.join);
-router.post('/move', GameCtrl.move);
+router.post("/createGame", GameCtrl.create);
+router.post("/joingame", GameCtrl.join);
+router.post("/move", GameCtrl.move);
 
 // get all game ids
 router.get("/fetchgameids", GameCtrl.get_game_ids);
 
 // temporary to test db function
-router.post('/dbWrite', UserCtrl.userCreate);
+router.post("/dbWrite", UserCtrl.userCreate);
 
 // temporary to test db function
-router.get('/dbRead', UserCtrl.getAllUsers);
+router.get("/dbRead", UserCtrl.getAllUsers);
 
 // temporary to test db function
 router.put("/dbUpdate/:id", UserCtrl.userUpdate);
@@ -31,10 +31,10 @@ router.put("/dbUpdate/:id", UserCtrl.userUpdate);
 router.get("/dbGameResult", ResultCtrl.createGameResult);
 
 // update game with result id in db
-router.patch('/dbUpdateResult', ResultCtrl.updateGameResult);
+router.patch("/dbUpdateResult", ResultCtrl.updateGameResult);
 
-router.get('/ping', (req, res) => {
-    res.json({ message: 'Hello from server!' });
+router.get("/ping", (req, res) => {
+  res.json({ message: "Hello from server!" });
 });
 
 // Export Module
