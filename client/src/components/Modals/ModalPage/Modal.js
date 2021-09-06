8e66b71e06162e90448bbbd3e3e6fd9e861f951f
Wavist <sommy.okafor@yahoo.com>
2021-09-06 14:06:54 +0100

    added game over modal to modalpage

--- client/src/components/Modals/ModalPage/Modal.js
+++ client/src/components/Modals/ModalPage/Modal.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import CongratulationsModal from "../CongratulationsModal/CongratulationsModal";
+import GameOverModal from "../GameOverModal/GameOver"
 
 export default function Modal() {
   const [show, setShow] = useState(false);
@@ -7,6 +8,9 @@ export default function Modal() {
     <div>
       <button onClick={() => setShow(true)}>Show Congratulations Modal</button>
       <CongratulationsModal onClose={() => setShow(false)} show={show} />
-    </div>
+
+      <button onClick={() => setShow(true)}>Show Game-Over Modal</button>
+      <GameOverModal onClose={() => setShow(false)} show={show} />
+  </div>
   );
 }
