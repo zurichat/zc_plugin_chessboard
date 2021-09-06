489b2210c3df0850bcef933407bc778fa4156e98
eni4sure <eni4sure@users.noreply.github.com>
2021-09-06 09:59:44 +0000

    Backend EsLint Fixes

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  PORT: process.env.PORT || 5050,
-  SOCKER_KEY:
+    PORT: process.env.PORT || 5050,
+    SOCKER_KEY:
     process.env.SOCKER_TOKEN || "58c2400b-831d-411d-8fe8-31b6e337738b",
-  SOCKET_URL: process.env.SOCKET_URL || "https://realtime.zuri.chat/api",
+    SOCKET_URL: process.env.SOCKET_URL || "https://realtime.zuri.chat/api",
 };
--- server/src/models/Result.js
+++ server/src/models/Result.js
@@ -2,9 +2,9 @@ const Joi = require("joi");
 
 //  a test user schema tobe updated based on requirements
 const resultSchema = Joi.object({
-  result_id: Joi.string().guid({ version: "uuidv4" }),
-  game_id: Joi.string().required(),
-  winner_id: Joi.string().required(),
+    result_id: Joi.string().guid({ version: "uuidv4" }),
+    game_id: Joi.string().required(),
+    winner_id: Joi.string().required(),
 });
 
 module.exports = resultSchema;
