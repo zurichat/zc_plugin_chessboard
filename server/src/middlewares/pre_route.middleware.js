0cb116a8975c6885f34e5b1ac209f6d13ab0f645
Destiny Erhabor <destinterhabor6@gmail.com>
2021-09-06 16:07:49 +0100

    done

--- server/src/middlewares/pre_route.middleware.js
+++ server/src/middlewares/pre_route.middleware.js
@@ -8,27 +8,27 @@ const cors = require("cors");
 
 // Export Module
 module.exports = (app) => {
-  // enable CORS
-  app.use(
-    cors({
-      // Allow all Origins so we can use the live api for local testing
-      origin: ["*"],
-    })
-  );
+    // enable CORS
+    app.use(
+        cors({
+            // Allow all Origins so we can use the live api for local testing
+            origin: ["*"],
+        })
+    );
 
-  // Secure the app by setting various HTTP headers off.
-  // app.use(helmet());
+    // Secure the app by setting various HTTP headers off.
+    // app.use(helmet());
 
-  // Tell express to recognize the incoming Request Object as a JSON Object
-  app.use(express.json());
+    // Tell express to recognize the incoming Request Object as a JSON Object
+    app.use(express.json());
 
-  // Express body parser
-  app.use(express.urlencoded({ extended: true }));
+    // Express body parser
+    app.use(express.urlencoded({ extended: true }));
 
-  // Create express static engine to run React app build
-  app.use(
-    express.static(path.join(__dirname, "..", "..", "..", "client", "build"))
-  );
+    // Create express static engine to run React app build
+    app.use(
+        express.static(path.join(__dirname, "..", "..", "..", "client", "build"))
+    );
 
-  return app;
+    return app;
 };
--- server/src/socket/end_game_handler.socket.js
+++ server/src/socket/end_game_handler.socket.js
@@ -3,25 +3,25 @@ const formatMessage = require("./libs/formatMessage");
 
 const end_game_handler =
   (socket) =>
-  ({ user_id, game_id }) => {
-    // random chess room
-    (user_id = "Bla BLaa"),
-      // broadcast message to all users in the chessroom  game_id
-      socket.broadcast
-        .to(game_id)
-        .emit(
-          "message",
-          formatMessage(user_id, `Game has ended in room ${game_id}`)
-        );
+      ({ user_id, game_id }) => {
+          // random chess room
+          (user_id = "Bla BLaa"),
+          // broadcast message to all users in the chessroom  game_id
+          socket.broadcast
+              .to(game_id)
+              .emit(
+                  "message",
+                  formatMessage(user_id, `Game has ended in room ${game_id}`)
+              );
 
-    // Close socket connection
-    //socket.close(game_id)
+          // Close socket connection
+          //socket.close(game_id)
 
-    // Welcome current user to the room
-    socket.emit(
-      "message",
-      formatMessage(user_id, `Game Over in chess room ${game_id}`)
-    );
-  };
+          // Welcome current user to the room
+          socket.emit(
+              "message",
+              formatMessage(user_id, `Game Over in chess room ${game_id}`)
+          );
+      };
 
 module.exports = end_game_handler;
