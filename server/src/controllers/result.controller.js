48add1dd559d98f7c083b85a7fd19412cf3d32af
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2021-09-07 22:54:07 +0100

    fixed all eslint errors

--- server/src/controllers/result.controller.js
+++ server/src/controllers/result.controller.js
@@ -1,9 +1,9 @@
 // Custom Modules
-const response = require("../utils/response");
-const CustomError = require("../utils/custom-error");
-const DatabaseConnection = require("../db/database.helper");
+//const response = require("../utils/response");
+//const CustomError = require("../utils/custom-error");
+//const DatabaseConnection = require("../db/database.helper");
 
-const ResultRepo = new DatabaseConnection("001test_result");
+//const ResultRepo = new DatabaseConnection("001test_result");
 class ResultController {
   // Get All Results
   // async getAll(req, res) {
--- server/src/routes/result.route.js
+++ server/src/routes/result.route.js
@@ -2,7 +2,7 @@
 const router = require("express").Router();
 
 // Custom Modules
-const ResultCtrl = require("../controllers/result.controller");
+//const ResultCtrl = require("../controllers/result.controller");
 
 // Get Result for all games - not implemented -- Ifechukwu Uzukwu
 // router.get("/all", ResultCtrl.getAll);
