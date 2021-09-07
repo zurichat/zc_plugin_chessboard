4ab804f9943d086a31cfb032c54abbf4566ac226
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2021-09-07 22:41:10 +0100

    finished distributing routes

--- server/src/routes/game.route.js
+++ server/src/routes/game.route.js
@@ -13,19 +13,19 @@ router.post("/join", GameCtrl.join);
 // Get All Games
 router.get("/all", GameCtrl.getAll);
 
-// Watch a Game - Not implemented
+// Watch a Game - Not implemented -- Eniola
 // router.post('/watch', GameCtrl.addSpectator)
 
-// End Game - Not Implemented
+// End Game - Not Implemented -- AfricanDev
 // router.post("/end", GameCtrl.endGame)
 
-// Resign  - Not implemented
+// Resign  - Not implemented -- Ace Anyanwu
 // router.post('/resign', GameCtrl.resign)
 
-// Get Game By Id - Not implemented
+// Get Game By Id - Not implemented -- Moses Odunosho
 // router.get("/:id", GameCtrl.getById);
 
-// Get All Games by User - not implemented
+// Get All Games by User - not implemented -- shauib mustapha
 // router.get("/all/:userId", GameCtrl.getAllByUser);
 
 // Export Module
--- server/src/routes/result.route.js
+++ server/src/routes/result.route.js
@@ -4,10 +4,10 @@ const router = require("express").Router();
 // Custom Modules
 const ResultCtrl = require("../controllers/result.controller");
 
-// Get Result for all games - not implemented
+// Get Result for all games - not implemented -- Ifechukwu Uzukwu
 // router.get("/all", ResultCtrl.getAll);
 
-// Get Result for a particular game - not implemented
+// Get Result for a particular game - not implemented -- Erhabor Destiny
 // router.get("/:gameId", ResultCtrl.getById);
 
 // Export Module
