926308691563f0304cab16df08d89aa47c719142
fabianuzukwu <fabianuzukwu@gmail.com>
2021-09-08 20:49:48 +0100

    router for get all  game results

--- server/src/controllers/result.controller.js
+++ server/src/controllers/result.controller.js
@@ -1,13 +1,22 @@
 // Custom Modules
-//const response = require("../utils/response");
-//const CustomError = require("../utils/custom-error");
-//const DatabaseConnection = require("../db/database.helper");
+const response = require("../utils/response");
+// const CustomError = require("../utils/custom-error");
+const DatabaseConnection = require("../db/database.helper");
 
-//const ResultRepo = new DatabaseConnection("001test_result");
+const ResultRepo = new DatabaseConnection("001test_result");
 class ResultController {
   // Get All Results
-  // async getAll(req, res) {
-  // }
+  async getAll(req, res, next) {
+    req;
+    try {
+       const resultsDBData = await ResultRepo.fetchAll();
+      res
+        .status(200)
+        .send(response("Results retrieved successfully", resultsDBData.data))        
+    } catch(error){
+      next(`Unable to get all Results: ${error}`);
+    }
+  }
   // Get A Result
   // async getById(req, res) {
   // }
--- server/src/routes/result.route.js
+++ server/src/routes/result.route.js
@@ -2,10 +2,10 @@
 const router = require("express").Router();
 
 // Custom Modules
-//const ResultCtrl = require("../controllers/result.controller");
+const ResultCtrl = require("../controllers/result.controller");
 
 // Get Result for all games - not implemented -- Ifechukwu Uzukwu
-// router.get("/all", ResultCtrl.getAll);
+router.get("/all", ResultCtrl.getAll);
 
 // Get Result for a particular game - not implemented -- Erhabor Destiny
 // router.get("/:gameId", ResultCtrl.getById);
