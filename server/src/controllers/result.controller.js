8bc2457aaca94f8e68e988f4c99542dc5f4cfedb
eni4sure <eni4sure@users.noreply.github.com>
2021-09-10 11:29:06 +0000

    same_commit

--- server/src/controllers/result.controller.js
+++ server/src/controllers/result.controller.js
@@ -11,21 +11,19 @@ class ResultController {
     const games = await GameRepo.fetchAll();
 
     try {
-        let resultsDBData = games.map((game)=>{
-          return {
-            // eslint-disable-next-line no-constant-condition
-            result: game.is_owner_winner = null ? "Draw" : "Win",
-            winner: game.is_owner_winner ? game.winner : game.opponent,
-            game_id: game._id
-
-          };
-        });
-
+      let resultsDBData = games.map((game) => {
+        return {
+          // eslint-disable-next-line no-constant-condition
+          result: (game.is_owner_winner = null ? "Draw" : "Win"),
+          winner: game.is_owner_winner ? game.winner : game.opponent,
+          game_id: game._id,
+        };
+      });
 
       res
         .status(200)
-        .send(response("Results retrieved successfully", resultsDBData));        
-    } catch(error){
+        .send(response("Results retrieved successfully", resultsDBData));
+    } catch (error) {
       throw new CustomError(`Unable to get all Results: ${error}`, 500);
     }
   }
