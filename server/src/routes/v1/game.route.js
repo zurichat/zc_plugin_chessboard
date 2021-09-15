441a51dda72e76dd98cd103f2f0ad9377f59fa5a
tobybrave <leutoby@gmail.com>
2021-09-15 01:38:51 +0100

    add comment route docs

--- server/src/routes/v1/game.route.js
+++ server/src/routes/v1/game.route.js
@@ -124,6 +124,24 @@ const GameCtrl = require("../../controllers/game.controller");
  *        description: The id the game user wants to forfeit
  *        required: true
  *
+ *   comment:
+ *    type: object
+ *    properties:
+ *     comment:
+ *      type: string
+ *      example: move the queen to protect the king
+ *      required: true
+ *     user_id:
+ *      type: string
+ *      description: The user id of the spectator
+ *      required: true
+ *      example: 613c960542a4c7d43b4cd2ad
+ *     game_id:
+ *      type: string
+ *      description: Game ID spectator is commenting on
+ *      example: 613b72eb3ce841615903e676
+ *      required: true
+ *
  */
 
 // Create A Game
@@ -334,8 +352,31 @@ router.get("/:id", GameCtrl.getById);
  */
 router.get("/all/:userId", GameCtrl.getAllByUser);
 
-// Send messages to game -- NotImplemented
-router.patch("/message",GameCtrl.comment);
+// Send comment during game
+/**
+ * @swagger
+ * /api/v1/game/comment:
+ *  patch:
+ *   summary: Update game comments
+ *   description: have conversations in the comment section of a game
+ *   requestBody:
+ *    required: true
+ *    content:
+ *      application/json:
+ *        schema:
+ *          $ref: '#/definitions/comment'
+ *
+ *   responses:
+ *    202:
+ *      description: comment sent
+ *    400:
+ *      description: comment cannot be empty
+ *    404:
+ *      description: Game not found
+ *    500:
+ *      description: Unable to Connect to Zuri Core DB
+ */
+router.patch("/comment",GameCtrl.comment);
 
 // Export Module
 module.exports = router;
