7068e53280ddcb36a6af86f69f444f86bede32f4
Watermelon-Sugar <Watermelon-Sugar@users.noreply.github.com>
2021-09-15 08:07:01 +0000

    same_commit

--- client/src/components/Modals/ForfeitModal/Forfeit.js
+++ client/src/components/Modals/ForfeitModal/Forfeit.js
@@ -1,4 +1,3 @@
-
 import Logo from "./profile.svg";
 import "./Forfeit.css";
 import { useHistory } from "react-router-dom";
@@ -13,7 +12,7 @@ const Forfeit = ({ isYes, handleClick }) => {
     <div className="forfeit-container">
       <div className="forfeit-modal">
         <div className="forfeit-header">
-            <img className="profile" src={Logo} alt="profile" />
+          <img className="profile" src={Logo} alt="profile" />
         </div>
 
         <div className="forfeit-content">
@@ -23,23 +22,20 @@ const Forfeit = ({ isYes, handleClick }) => {
         </div>
         <footer className="forfeit-footer">
           <div className="cp">
-            <button
-            className=" btn continue"
-            onClick={() => handleClick()}
-          >
-            Continue Playing
-          </button>
+            <button className=" btn continue" onClick={() => handleClick()}>
+              Continue Playing
+            </button>
           </div>
-          
+
           <div className="ys">
             <button
-            className="btn yes"
-            onClick={() => {
-              history.push("/");
-            }}
-          >
-            Yes
-          </button>
+              className="btn yes"
+              onClick={() => {
+                history.push("/");
+              }}
+            >
+              Yes
+            </button>
           </div>
         </footer>
       </div>
