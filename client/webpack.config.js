84e0f7d41cba2d9b732e94f910e8bd9a7179a851
odizee <odizee@users.noreply.github.com>
2021-09-16 12:50:34 +0000

    same_commit

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,20 +2,15 @@ const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react");
 
 module.exports = (webpackConfigEnv, argv) => {
-  
   const defaultConfig = singleSpaDefaults({
     orgName: "zuri",
     projectName: "zuri-plugin-chessboard",
     webpackConfigEnv,
     argv,
-    
   });
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
-    devServer: { historyApiFallback: true, },
+    devServer: { historyApiFallback: true },
   });
-
-  
 };
-
