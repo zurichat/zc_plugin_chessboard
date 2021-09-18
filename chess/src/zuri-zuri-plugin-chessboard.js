57a497e29b750a7f82bba917e48049d9d43694d7
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2021-09-18 12:26:22 +0100

    removed dotenv file

--- chess/src/zuri-zuri-plugin-chessboard.js
+++ chess/src/zuri-zuri-plugin-chessboard.js
@@ -2,10 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import App from "./App.js";
-import dotenv from "dotenv";
 import "./index.css";
-
-dotenv.config();
 
 const lifecycles = singleSpaReact({
   React,
