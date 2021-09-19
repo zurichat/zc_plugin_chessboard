2df4c5121a450ec3b39be0a784348eaba227c75b
Paul Keno <paulkenoofficial.com>
2021-09-19 12:42:53 +0100

    another change from upstream

--- chess/src/Pages/LandingPage/LandingPage.js
+++ chess/src/Pages/LandingPage/LandingPage.js
@@ -5,13 +5,8 @@ import { Link } from "react-router-dom";
 const LandingPage = () => {
     return ( 
         <div className="landing-page">
-<<<<<<< HEAD
-                <img src={ CoverImage } alt="cover image" />
-                <Link to = "/chess"><button className="landing_page_button" > PROCEED TO APP</button></Link>
-=======
                 <div className="image"><img src={ CoverImage } alt="cover image" /></div>
                 <Link to = "/chess"><button>PROCEED TO APP</button></Link>
->>>>>>> a0100ac5bf2712ccad668c2a9d4cf5cc7ac19817
         </div>
      );
 };
