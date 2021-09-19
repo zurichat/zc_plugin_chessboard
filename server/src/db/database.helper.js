5054bbf1bffae69110da2c1ba5190c4ff91ae8f7
Eniola Osabiya <eosabiya@gmail.com>
2021-09-19 01:07:51 +0100

    removed unused console.logs

--- server/src/db/database.helper.js
+++ server/src/db/database.helper.js
@@ -146,7 +146,6 @@ class DatabaseConnection {
       );
 
       // Return the response
-      console.log(response.data);
       return response.data;
     } catch (error) {
       throw new CustomError(
