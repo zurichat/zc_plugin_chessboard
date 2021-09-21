37b04b78aad0db23c17fb5360073d0a1cf4a22df
Odizee <odizeeplatform@gmail.com>
2021-09-20 21:07:36 -1100

    fixed game comment and no comment conflicts

--- chess/src/App.js
+++ chess/src/App.js
@@ -39,7 +39,7 @@ function App() {
 
           <Route
             exact
-            path="/game_comments"
+            path="/game_comments:id"
             render={() => <GameScreenWithComments />}
           />
 
