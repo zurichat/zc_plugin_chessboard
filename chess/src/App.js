56b449a7a18c0825e99791b433a55e82b61ce861
Eniola Osabiya <eosabiya@gmail.com>
2021-09-22 04:07:35 +0100

    webpack fix

--- chess/src/App.js
+++ chess/src/App.js
@@ -5,22 +5,22 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 // Import Pages
-import Homepage from "./pages/Home";
-import Game from "./pages/Game";
-import Rules from "./pages/Rules";
+// import Homepage from "./pages/Home";
+// import Game from "./pages/Game";
+// import Rules from "./pages/Rules";
 
 function App() {
   return (
     <Router basename="/chess">
       <Switch>
         {/* Home Page/ View Board Games in Organisation */}
-        <Route exact path="/" component={Homepage} />
+        {/* <Route exact path="/" component={Homepage} /> */}
 
         {/* Game Page/ Play a Game/ Spectator View */}
-        <Route exact path="/game/:game_id" component={Game} />
+        {/* <Route exact path="/game/:game_id" component={Game} /> */}
 
         {/* Rules Page */}
-        <Route exact path="/rules" component={Rules} />
+        {/* <Route exact path="/rules" component={Rules} /> */}
       </Switch>
     </Router>
   );
