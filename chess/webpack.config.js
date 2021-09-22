8e61ee01d63fc27721b94b264b82c07847301b93
Eniola Osabiya <eosabiya@gmail.com>
2021-09-22 01:26:46 +0100

    fix test for webpack fail

--- chess/webpack.config.js
+++ chess/webpack.config.js
@@ -12,7 +12,6 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
-    context: path.resolve(__dirname, 'src'),
     devServer: {
       historyApiFallback: true,
       headers: {
