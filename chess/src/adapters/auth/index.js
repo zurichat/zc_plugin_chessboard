acfba68b09f742087faaba8d8b30b732f2d02908
e-nicholas987 <60387740+e-nicholas987@users.noreply.github.com>
2021-09-23 08:52:20 +0100

    join game func fix

--- chess/src/adapters/auth/index.js
+++ chess/src/adapters/auth/index.js
@@ -1,21 +1,21 @@
 // Import api call adapter
 import { get } from "../xhr";
 import axios from "axios";
-// import { GetUserInfo } from "@zuri/zuri-control";
+import { GetUserInfo } from "@zuri/zuri-control";
 
 async function login() {
-  // const { data } = await axios.post("https://api.zuri.chat/auth/login", {
-  //   email: "eosabiya@gmail.com",
-  //   password: "password"
-  // });
+  const { data } = await axios.post("https://api.zuri.chat/auth/login", {
+    email: "eosabiya@gmail.com",
+    password: "password"
+  });
 
-  // return data;
+  return data;
 }
 
 export async function zc_main_user_info() {
-  // const login_info = await login();
-  // console.log(login_info);
-  // console.log(GetUserInfo());
+  const login_info = await login();
+  console.log(login_info);
+  console.log(GetUserInfo());
 }
 
 export function getLoggedInUserData() {
