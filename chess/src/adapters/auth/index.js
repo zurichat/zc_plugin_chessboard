774ea9bb01dd654a0f4a4d429e1451a75eaf1c68
Eniola Osabiya <eosabiya@gmail.com>
2021-09-24 20:27:35 +0100

    zc_main finally implemented

--- chess/src/adapters/auth/index.js
+++ chess/src/adapters/auth/index.js
@@ -3,23 +3,20 @@ import { get } from "../xhr";
 import axios from "axios";
 import { GetUserInfo } from "@zuri/control";
 
-export function getLoggedInUserData() {
-  
-  GetUserInfo().then(user => {
-    console.log(user);
-  });
+export async function getLoggedInUserData() {
+  const logged_in_user_from_zc_main = await GetUserInfo();
 
-  return {
-    user_id: "new_player_one_user_id",
-    user_name: "NewcodeJonin",
-    image_url: "https://www.gravatar.com/avatar/",
-  };
-}
-
-export function testPlayerTwoData() {
-  return {
-    user_id: "new_player_two_user_id",
-    user_name: "michael",
-    image_url: "https://www.gravatar.com/avatar/",
-  };
+  if (location.hostname === "localhost" || location.hostname === "127.0.0.1"){
+    return {
+      user_id: "new_player_one_user_id",
+      user_name: "NewcodeJonin",
+      image_url: "https://www.gravatar.com/avatar/",
+    };
+  } else {
+    return {
+      user_id: logged_in_user_from_zc_main.user_id,
+      user_name: logged_in_user_from_zc_main.first_name + " " + logged_in_user_from_zc_main.last_name,
+      image_url: `https://ui-avatars.com/api/?name=${logged_in_user_from_zc_main.first_name}&background=random&uppercase=false`,
+    };
+  }  
 }
