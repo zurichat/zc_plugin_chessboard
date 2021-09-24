590bfba45cf6df780d9dc68bafff88c7b84c4ee6
Eniola Osabiya <eosabiya@gmail.com>
2021-09-24 22:56:31 +0100

    await in async hack

--- chess/src/adapters/auth/index.js
+++ chess/src/adapters/auth/index.js
@@ -1,23 +1,34 @@
 // Import api call adapter
 import { get } from "../xhr";
-import axios from "axios";
 import { GetUserInfo } from "@zuri/control";
 
-export async function getLoggedInUserData() {
-  const logged_in_user_from_zc_main = await GetUserInfo();
+export function getLoggedInUserData() {
 
-  if (location.hostname === "localhost" || location.hostname === "127.0.0.1"){
+  // Variable to get User Info Data - Mocking Global Variables
+  let logged_in_user_from_zc_main;
+
+  // Try to set user info from ZC_Main
+  (async function () {
+    logged_in_user_from_zc_main = await GetUserInfo();
+  })();
+
+  // Localhost can't use the auth function
+  if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+
+    // Just return hardcoded info
     return {
       user_id: "localhost_user_id",
       user_name: "LocalhostUser",
       image_url: "https://www.gravatar.com/avatar/",
     };
   } else {
-    console.log(logged_in_user_from_zc_main);
-    return {
-      user_id: logged_in_user_from_zc_main._id,
-      user_name: logged_in_user_from_zc_main.first_name + " " + logged_in_user_from_zc_main.last_name,
-      image_url: `https://ui-avatars.com/api/?name=${logged_in_user_from_zc_main.first_name}&background=random&uppercase=false`,
-    };
-  }  
+    // Set timeout to let await resolve (the only hack i could find to make await work in synchronous function)
+    setTimeout(() => {
+      return {
+        user_id: logged_in_user_from_zc_main._id,
+        user_name: logged_in_user_from_zc_main.first_name + " " + logged_in_user_from_zc_main.last_name,
+        image_url: `https://ui-avatars.com/api/?name=${logged_in_user_from_zc_main.first_name}&background=random&uppercase=false`,
+      };
+    })
+  }
 }
