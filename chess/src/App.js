d90ebdbb3f1c90ff5584c0b8b74f9f9afb7e3b2f
otutukingsley <56305995+otutukingsley@users.noreply.github.com>
2021-09-28 15:14:45 -0700

    accepted incoming changes

--- chess/src/App.js
+++ chess/src/App.js
@@ -4,49 +4,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // Import the CSS
 import "./App.css";
 
-<<<<<<< HEAD
-// Zuri Cross Import
-// import { GetUserInfo } from "@zuri/zuri-control";
-
-function App() {
-  return (
-    <div className="App">
-      <Router basename="/chess">
-        <Switch>
-          {/* View All Games */}
-          <Route exact path="/" render={() => <HomePage />} />
-
-          {/* ZC Main Comm Test Page */}
-          {/* <Route
-            exact
-            path="/zc_main_test"
-            render={() => console.log(GetUserInfo())}
-          /> */}
-
-          {/* Specatator Game View */}
-          <Route exact path="/game/:id" render={() => <MainGame />} />
-
-          {/* Actual Game Page for Player 1 and Player 2 */}
-          <Route
-            exact
-            path="/game_nocomments/:id"
-            render={() => <GameScreenWithoutComments />}
-          />
-
-          <Route
-            exact
-            path="/game_comments"
-            render={() => <GameScreenWithComments />}
-          />
-
-          <Route exact path="/inviteplayer" render={() => <InviteModal />} />
-
-          {/* Rules Page */}
-          <Route exact path="/rules" render={() => <Rules />} />
-        </Switch>
-      </Router>
-    </div>
-=======
 // Import Pages
 import Homepage from "./Pages/Home";
 import Game from "./Pages/Game";
@@ -66,7 +23,6 @@ function App() {
         <Route exact path="/rules" component={Rules} />
       </Switch>
     </Router>
->>>>>>> 604891b638904d0e761f9bc08d7f0db570999813
   );
 }
 
