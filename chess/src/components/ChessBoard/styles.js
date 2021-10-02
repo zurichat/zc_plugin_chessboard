6d1a8dc5f04984cf6d9dace001c4e8568742cb99
Babsmutini <scotteyawo@gmail.com>
2021-10-02 14:46:50 +0100

    Quick fix commentbox

--- chess/src/components/ChessBoard/styles.js
+++ chess/src/components/ChessBoard/styles.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 
 export const ChessboardContainer = styled.div`
-  display: grid;
-  place-content: center;
-  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  padding: 0 4rem;
+  height: 100%;
   max-width: 655px;
   width: 90%;
   margin: 0 auto;
-  position: relative;
 `;
