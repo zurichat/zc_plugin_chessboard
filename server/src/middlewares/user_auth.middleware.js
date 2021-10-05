8bfff7187eef7771690732fe6f3070763d7aec77
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2021-10-05 13:39:20 +0100

    fixed user auth to allow sidebar query

--- server/src/middlewares/user_auth.middleware.js
+++ server/src/middlewares/user_auth.middleware.js
@@ -6,7 +6,12 @@ const { WELCOME_URL } = require("../config/index");
 exports.userAuth = async (req, res, next) => {
   try {
     const { user_id } = req.body;
-    if (user_id === "chessbot" || user_id === "anonymous") return next();
+    if (
+      user_id === "chessbot" ||
+      user_id === "anonymous" ||
+      (req.query.user && req.query.org)
+    )
+      return next();
 
     const authorization = req.get("Authorization");
     if (!authorization) throw new CustomError("Authentication failed", 403);
