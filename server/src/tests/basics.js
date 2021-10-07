e66999f74e43d74057dcff5726f0e623e8f585b5
tobybrave <leutoby@gmail.com>
2021-10-07 21:53:12 +0000

    healthcheck test

---
+++ server/src/tests/basics.js
@@ -0,0 +1,25 @@
+const chai = require("chai");
+const supertest = require("supertest");
+
+const server = require("../../index");
+
+const expect = chai.expect;
+const api = supertest(server);
+
+describe("checks", () => {
+  it("sanity checks", () => {
+    expect(10 * 10).to.equal(100);
+  });
+  
+  it("api health check", async () => {
+    // query to by-pass user auth
+    const response = await api.get("/api/v1/ping/?user=''&org=''")
+      .expect(200)
+      .expect("Content-Type", /application\/json/)
+    
+    expect(response.body).to.have.property("message");
+    expect(response.body.message).to.be.a("string");
+    expect(response.body.message).to.match(/hello from server/i);
+    
+  }, 500);
+})
