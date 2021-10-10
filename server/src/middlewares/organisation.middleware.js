52e89d0997104e32435677ed7d2c2dd8a15303a6
Eniola Osabiya <eosabiya@gmail.com>
2021-10-10 14:02:20 +0100

    removed auth from whole application and moved to game routes only, refactored org middleware

--- server/src/middlewares/organisation.middleware.js
+++ server/src/middlewares/organisation.middleware.js
@@ -1,33 +1,33 @@
-const response = require("../utils/response");
+const CustomError = require("../utils/custom-error");
 
-// Export Module
-module.exports = (app) => {
-  app.use(function (req, res, next) {
-    let organisation = req.headers.organisation;
+// Verify the organisationID passed in the request is valid
+exports.orgAuth = async (req, res, next) => {
+    try {
+        let { organisation } = req.headers;
 
-    // Verify if the organisation is valid
-    // OMO laterrr
+        // Verify if the organisation is valid
+        // OMO laterrr
 
-    // If org_id is not set in the header, then set it to the default organisation
-    if (
-      !organisation ||
-      organisation === null ||
-      organisation === "null" ||
-      organisation === "undefined"
-    ) {
-      // Use hardcoded organisation
-      res.locals.organisation_id = "6145c2d0285e4a18402073f6";
+        // If org_id is not set in the header, then set it to the default organisation
+        if (
+            !organisation ||
+            organisation === null ||
+            organisation === "null" ||
+            organisation === "undefined"
+        ) {
+            // Use hardcoded organisation
+            res.locals.organisation_id = "6145c2d0285e4a18402073f6";
 
-      // For now, we will use the default organisation
-      // res.status(400).send(response("Organisation header is required", null, false));
-    } else {
-      // Set the organisation id in req variable
-      res.locals.organisation_id = organisation;
+            // For now, we will use the default organisation
+            // res.status(400).send(response("Organisation header is required", null, false));
+        } else {
+            // Set the organisation id in req variable
+            res.locals.organisation_id = organisation;
+        }
+
+        // Continue to the next request
+        next();
+    } catch (error) {
+        throw new CustomError(`Orgnisation Middleware: ${error}`, 502);
     }
-
-    // Continue to the next request
-    next();
-  });
-
-  return app;
 };
