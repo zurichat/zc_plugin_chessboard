43b885628ba02b12f3188168f8a5d352e755e5dc
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2021-11-03 11:56:58 +0100

    changed default port to 22664

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 module.exports = {
   // Port to run the server on
-  PORT: process.env.PORT || 5050,
+  PORT: process.env.PORT || 22664,
 
   // Cetrifugo Credentials
   SOCKER_KEY: process.env.SOCKER_TOKEN || '58c2400b-831d-411d-8fe8-31b6e337738b',
