34309ae4e17616230c8cddba5bb2d70cbaa2ddbd
ridowayne <hassanrilwan12@gmail.com>
2022-11-27 19:58:28 +0100

    [Bug]:tested and fixed generate image error

--- backend/src/middlewares/user_auth.middleware.js
+++ backend/src/middlewares/user_auth.middleware.js
@@ -10,7 +10,7 @@ exports.userAuth = async (req, res, next) => {
     const member_id = req.query.member_id || req.params.member_id;
     // Also get user_id from query for get requests
     if (!user_id) {
-      user_id = 'anonymous';
+      user_id = req.query.user_id;
     }
 
     if (
